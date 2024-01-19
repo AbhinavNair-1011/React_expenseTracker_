5ab3681c1e283338467f6be1b237ba3fc0dda666
AbhinavNair-1011 <abhivish1011@gmail.com>
2024-01-19 04:12:16 -0800

    conditional rendering

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,13 +7,15 @@ function ExpenseItem(props) {
   return (
     <div>
         <ul className="expenseList">
+
+          {props.expenses.length ==0 && <p>No expenses yet</p>}
           
         {props.expenses.map((item)=>(
 
                 <li key={item.expensePrice} className='expenseList_expenses'><span className="expenseNameSpan">{item.expenseName}</span> 
                 <span className="expensePriceSpan ">{item.expensePrice}</span> <DeleteButton deleteExpense={props.deleteExpense}/></li>
                
-        ))}
+        )) }
         </ul>
     </div>
   )
