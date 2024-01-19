749a3ea87db55cfcf546b629a35c74a3d2748ba7
AbhinavNair-1011 <abhivish1011@gmail.com>
2024-01-19 03:45:53 -0800

    managing multiple state in a single state

--- src/components/ExpenseInput.js
+++ src/components/ExpenseInput.js
@@ -5,17 +5,26 @@ function ExpenseInput(props) {
   const [expenseName,setExpenseName]=useState("");
   const [expensePrice,setExpensePrice]=useState('');
 
+  const [userInput,setUserInput]=useState({
+    expenseName:"",
+    expensePrice:""
+  })
+
   const submitHandler=(e)=>{
     e.preventDefault()
-    props.addExpense({expenseName,expensePrice})
+    props.addExpense(userInput)
   }
   return (
     <div>
           <form className='expenseInput' onSubmit={submitHandler}>
         <label htmlFor="expenseInput_itemName">Enter the Expense Name</label>
-            <input type="text" className='expenseInput_expenseNameInput' onChange={(e)=>{setExpenseName(e.target.value)}}/>
+            <input type="text" className='expenseInput_expenseNameInput' onChange={(e)=>{setUserInput(prevState=>{
+              return {...prevState,expenseName:e.target.value}
+            })}}/>
             <label htmlFor="expenseInput_itemPrice">Enter the Expense Price</label>
-            <input type="number" className='expenseInput_expensePriceInput' onChange={(e)=>{setExpensePrice(e.target.value)}}/>
+            <input type="number" className='expenseInput_expensePriceInput' onChange={(e)=>{setUserInput(prevState=>{
+              return {...prevState,expensePrice:e.target.value}
+            })}}/>
             <input type="submit" className="expenseInput_submit" />
         </form>
     </div>
